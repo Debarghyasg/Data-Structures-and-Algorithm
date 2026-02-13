bf77491383bc83b784973bfe9869642917eeb632
Debarghyasg <debarghya817@gmail.com>
2026-02-13 23:41:52 +0530

    recursion using backtrack

---
+++ recursion_backtrack.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class f3{
+    public void f5(int i,int n){
+        if(i<1){
+            return;
+        }
+        f5(i-1,n);
+        System.out.println(i);
+    }
+
+}
+
+
+public class recursion_backtrack {
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      f3 obj=new f3();
+      obj.f5(10,1);
+     
+    
+}
+}
