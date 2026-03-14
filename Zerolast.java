502bfeeecb250d591c1631634c7b8b8dee3bb299
Debarghyasg <debarghya817@gmail.com>
2026-03-14 19:31:39 +0530

    zero to the last array solution modified

---
+++ Zerolast.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class last{
+    void lastzero2(int[] arr,int n){
+        int i=0;
+        for(int j=0;j<arr.length;j++){
+            if(arr[j]!=0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                
+                i++;
+            }
+
+        }
+    }
+}
+
+public class Zerolast {
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the number of terms in array:");
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+
+        }
+        last obj=new last();
+        obj.lastzero2(arr,n);
+        
+        for(int i=0;i<n;i++){
+            System.out.print(arr[i]);
+
+        }
+        
+
+
+
+
+    }
+}
