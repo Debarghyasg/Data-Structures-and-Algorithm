6a8a49297cd13bd858e40424090ca85df57b0761
Debarghyasg <debarghya817@gmail.com>
2026-03-22 21:53:19 +0530

    Sort an array of 0s,1s,2s.

--- sort012.java
+++ sort012.java
@@ -1,10 +1,20 @@
 import java.util.*;
+class sort{
+    void sort1(int[] arr,int n){
+        
+
+    }
+}
 public class sort012 {
     public static void main(String[] args){
         Scanner sc=new Scanner(System.in);
         System.out.print("Enter the number of terms of an array:");
         int n=sc.nextInt();
+        int[] arr=new int[n];
         System.out.print("Enter the array:");
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
         
 
     }
