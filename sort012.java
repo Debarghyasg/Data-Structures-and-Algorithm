f895b53b35dd3038284ab1819ba2d17bb3cca2f1
Debarghyasg <debarghya817@gmail.com>
2026-03-23 17:29:30 +0530

    Sort an array of 0s,1s,2s.(TC=O(N)

--- sort012.java
+++ sort012.java
@@ -1,8 +1,32 @@
 import java.util.*;
 class sort{
-    void sort1(int[] arr,int n){
-        
+    int[] sort1(int[] arr,int n){
+        int i=0;
+        int low=i;
+        int mid=i;
+        int high=arr.length-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                int c=arr[mid];
+                arr[mid]=arr[low];
+                arr[low]=c;
+                mid++;
+                low++;
 
+
+
+            }
+            else if(arr[mid]==1){mid++;}
+            else if(arr[mid]==2){
+                int d=arr[mid];
+                arr[mid]=arr[high];
+                arr[high]=d;
+
+                high--;}
+            
+            
+        }
+        return arr;
     }
 }
 public class sort012 {
@@ -12,9 +36,15 @@ public class sort012 {
         int n=sc.nextInt();
         int[] arr=new int[n];
         System.out.print("Enter the array:");
-        for(int i=0;i<n;i++){
+        for(int i=0;i<arr.length;i++){
             arr[i]=sc.nextInt();
         }
+        sort obj=new sort();
+        obj.sort1(arr,n);
+        for(int i=0;i<arr.length;i++){
+        System.out.print(arr[i]); 
+        }
+        
         
 
     }
