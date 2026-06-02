fb20146d463b5dcf3a091e3ccbb4bff05e0ae113
Debarghyasg <debarghya817@gmail.com>
2026-06-02 21:53:01 +0530

    Preorder traversal

---
+++ Trees/preorder.java
@@ -0,0 +1,37 @@
+class Node{
+    int data;
+    Node left,right;
+    public Node(int item){
+        data=item;
+         left=right=null;
+
+        
+    }
+}
+public class preorder{
+    Node root;
+
+    
+    void preorder2(Node node){
+        if(node==null){
+        return;
+    }
+    System.out.print(node.data);
+    preorder2(node.left);
+    preorder2(node.right);
+
+    }
+    public static void main(String[] args){
+        preorder tree=new preorder();
+        tree.root=new Node(1);
+
+        tree.root.left=new Node(2);
+        tree.root.right=new Node(3);
+        System.out.println("The binary tree is:");
+        tree.preorder2(tree.root);
+
+
+
+    }
+
+}
